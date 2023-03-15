a75b9cdc3d456093918c8bbbb0667fb36088650c
andreacasanova1 <116755988+andreacasanova1@users.noreply.github.com>
2023-03-15 18:34:56 +0100

    menú buerger ok

--- JS/index.js
+++ JS/index.js
@@ -2,15 +2,18 @@
 document.querySelector(".menu-burger").addEventListener("click" , animateBurger) //crear esta funcion animateBurger
 //antes hay que crear variables para las barras
 
-let line1__burger = document.querySelector("line1__menu-burger");
-let line2__burger = document.querySelector("line2__menu-burger");
-let line3__burger = document.querySelector("line3__menu-burger");
+let line1__burger = document.querySelector(".line1__menu-burger");
+let line2__burger = document.querySelector(".line2__menu-burger");
+let line3__burger = document.querySelector(".line3__menu-burger");
+let topbar = document.querySelector(".topbar");
 //comandos rápidos, si dejo presionado (alt) y marco los puntos que quiero
 //modificar se cambian simultaneamente.
 
 //ahora crear la funcion
 function animateBurger() {
-    line1__burger.classList.toggle(".activeline1__menu-burger");
-    line2__burger.classList.toggle(".activeline2__menu-burger");
-    line3__burger.classList.toggle(".activeline3__menu-burger");
+    line1__burger.classList.toggle("activeline1__menu-burger");
+    line2__burger.classList.toggle("activeline2__menu-burger");
+    line3__burger.classList.toggle("activeline3__menu-burger");
+    topbar.classList.toggle("topbar__menu");
 }
+
